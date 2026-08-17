daca18de5c8b2ce4ffdf4cab62d5a1c5455d3288
jan-lindner00 <mail.jnvpn@gmail.com>
2026-08-17 07:49:30 +0200

    fix: changed height of line breaks in projects page

--- components/TextSplitter.tsx
+++ components/TextSplitter.tsx
@@ -1,14 +1,12 @@
-import { Fragment } from "react/jsx-runtime"
-
 export default function TextSplitter({text}: {text: string}){
     return (
         <>
             {text.split("<br>").map(p => {
                 return (
-                    <Fragment key={p}>
+                    <div key={p}>
                         <p className="text-small md:text-pdesktop opacity-80 font-inter max-w-250">{p}</p>
-                        <br></br>
-                    </Fragment>
+                        <br className="block content-[''] mt-2 md:mt-[1.25rem]"></br>
+                    </div>
                 )
             })}
         </>
